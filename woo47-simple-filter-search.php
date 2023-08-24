891603f2847aa2062a919b7ae3ba675b4e94ef2c
munesudube <dev.munesu@gmail.com>
2023-08-24 09:17:50 +0200

    Barebones plugin with name and version

---
+++ woo47-simple-filter-search.php
@@ -0,0 +1,8 @@
+<?php
+    /*
+        Plugin Name: Woo47 Simple Filter Search
+        Description: A plugin to add a simple dropdown filtered search to your woocommerce store
+        Version: 0.0.0
+        Author: Munesu Tembo Dube
+    */
+?>
